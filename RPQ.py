36e4fe166621590aa91b181413a9c0d3644db6f6
vizels <mirchuk.georgiy@gmail.com>
2024-03-10 23:00:51 +0100

    sortR (RQ doesn't work). Updated gitignore

--- RPQ.py
+++ RPQ.py
@@ -14,6 +14,8 @@ class RPQ:
             
             data = content[index + 2 : index + 2 + rows_num]
             
+        self.rows_num = rows_num
+
         for i in range(len(data)):
             # remove '\n' from the end of the line and split data by space
             data[i] = list(map(int, data[i][:-1].split(' ')))
@@ -45,22 +47,52 @@ class RPQ:
 
 
     def print_order(self, order):
-        print("Default order:")
+        print("Order:")
 
         for i in order:
-            print(order[i]+1, end = " ")
+            print(i+1, end = " ")
 
         print()
         # print("Order: ", *order, sep=' ')
         
 
     def find_optimal_order(self):
-        pass
-    
+        sorted_by_r = self.dataframe.sort_values(by = 'r')
+
+        sorted_by_q = self.dataframe.sort_values(by = 'q', ascending = False)
+
+        print(sorted_by_r)#, sorted_by_q, sep = '\n')
+ 
+        #create a dictionary with indexes of the rows in the dataframe and empty values
+        weights = dict.fromkeys(range(len(self.dataframe)), 0)
+        print(weights)
+
+        for i in range(self.rows_num):
+            r, q = sorted_by_r.index[i], sorted_by_q.index[i]
+
+            weights[r] += i
+            #weights[q] += i
+        
+        print(weights)
+        
+        #sort the dictionary by values
+        sorted_weights = sorted(weights.items(), key = lambda x: x[1])
+        print(sorted_weights)
+        
+        #print only the indexes of the rows
+        #print([x[0] for x in sorted_weights])
+
+        self.actual_order = [x[0] for x in sorted_weights]
+        print(self.actual_order)
+            
 
 if __name__ == "__main__":
     rpq = RPQ(1)
 
     rpq.print_order(rpq.default_order)
         
-    rpq.calculate_Cmax()    
+    rpq.calculate_Cmax()  
+
+    rpq.find_optimal_order()  
+
+    rpq.print_order(rpq.actual_order)
